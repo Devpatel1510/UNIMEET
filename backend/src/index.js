55f9d070cded9c499fbac272ed74070e2fa7bf9b
Dev Patel <devpatel05official@gmail.com>
2025-06-21 12:43:13 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,6 +30,14 @@ app.use("/api/messages", messageRoute);
 app.use("/api", otpRoutes);
 app.use("/api/account", accountRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 
 server.listen(PORT, () => {
